bd46d39961fd4d6070c3c686b2e1f8f364ead430
NackzosYT <41880498+NackzosYT@users.noreply.github.com>
2018-08-25 07:22:12 +0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -30,6 +30,23 @@ client.user.setGame(`Nothing`,"http://twitch.tv/nackzos")
 
 
 
+client.on('ready', function(){
+    var ms = 3000    ;
+    var setGame = ['v!help','v!help|v!inv','v!help|ViprBot','v!help|v!sup','v!help|Dev|Nackzos','ViprBot^-^'];
+    var i = -1;
+    var j = 0;
+    setInterval(function (){
+        if( i == -1 ){
+            j = 1;
+        }
+        if( i == (setGame.length)-1 ){
+            j = -1;
+        }
+        i = i+j;
+        client.user.setGame(setGame[i],`http://www.twitch.tv/nackzos`);
+    }, ms);
+
+});
 
 
 
