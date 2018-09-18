5b43c521cc8cf9d36e42b8022982f054310b0260
NackzosYT <41880498+NackzosYT@users.noreply.github.com>
2018-09-18 20:58:23 +0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -988,7 +988,6 @@ client.on('message', msg => {
   let args = msg.content.split(" ").slice(1);
 
     if(command === "&clear") {
-        const emoji = client.emojis.find("name", "wastebasket")
     let textxt = args.slice(0).join("");
     if(msg.member.hasPermission("MANAGE_MESSAGES")) {
     if (textxt == "") {
