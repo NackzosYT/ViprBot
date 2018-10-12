80a6142499795177034559349d12f928740185d3
NackzosYT <41880498+NackzosYT@users.noreply.github.com>
2018-10-12 18:03:32 +0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -34,8 +34,8 @@ client.on('message', message => {
     if(!message.channel.guild) return;
 let args = message.content.split(' ').slice(1).join(' ');
 if (message.content.startsWith('&bcall')){
-if (message.author.id !== '487333494910091267 ') return message.reply('** هذا الأمر قفط لصاحب البوت و شكراًً **')
-if(!message.author.id === '487333494910091267 ') return;
+if (message.author.id !== '487333494910091267') return message.reply('** هذا الأمر قفط لصاحب البوت و شكراًً **')
+if(!message.author.id === '487333494910091267') return;
 message.channel.sendMessage('جار ارسال |✅')
 client.users.forEach(m =>{
 m.sendMessage(args)
