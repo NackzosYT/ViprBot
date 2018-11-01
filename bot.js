d8c030a3a898a9a65d6dd77395d1597d295bd117
NackzosYT <41880498+NackzosYT@users.noreply.github.com>
2018-11-01 15:06:06 +0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,12 +10,12 @@ client.on('ready', async() => {
 var server = "507505319610417153"; 
 var channel = "507505319610417155";
     setInterval(()=>{
-    client.guilds.get(server).channels.get(channel).send('اكتب الرساله هنا')
+    client.guilds.get(server).channels.get(channel).send('ناكزوس وبس')
     },305);
 })
 
 client.on('message',function(message) {
-    let prefix = "!";
+    let prefix = "!&";
 let args = message.content.split(" ").slice(1).join(" ");
 if(message.content.startsWith(prefix + "say")) {
 if(!args) return;
