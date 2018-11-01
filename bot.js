12d788d096086f6da1b0466d27bf02d9a821be10
NackzosYT <41880498+NackzosYT@users.noreply.github.com>
2018-11-01 14:47:02 +0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,34 +1,26 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`Nine|Bank<3`,"http://twitch.tv/nackzos")
-  console.log('')
-  console.log('')
-  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
-  console.log(`[Start] ${new Date()}`);
-  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
-  console.log('')
-  console.log('╔[════════════════════════════════════]╗');
-  console.log(`Logged in as * [ " ${client.user.username} " ]`);
-  console.log('')
-  console.log('Informations :')
-  console.log('')
-  console.log(`servers! [ " ${client.guilds.size} " ]`);
-  console.log(`Users! [ " ${client.users.size} " ]`);
-  console.log(`channels! [ " ${client.channels.size} " ]`);
-  console.log('╚[════════════════════════════════════]╝')
-  console.log('')
-  console.log('╔[════════════]╗')
-  console.log(' Bot Is Online')
-  console.log('╚[════════════]╝')
-  console.log('')
-  console.log('')
+ console.log("welcome bot"); 
+console.log("log");
 });
 
+client.on('ready', async() => {
+var server = "507505319610417153"; 
+var channel = "507505319610417155";
+    setInterval(()=>{
+    client.guilds.get(server).channels.get(channel).send('اكتب الرساله هنا')
+    },305);
+})
 
+client.on('message',function(message) {
+    let prefix = "!";
+let args = message.content.split(" ").slice(1).join(" ");
+if(message.content.startsWith(prefix + "say")) {
+if(!args) return;
+message.channel.send(`${args}`); 
+}
+});
 
-
-client.login(process.env.BOT_TOKEN);
+client.login(process.env.BOT_TOKEN); 
